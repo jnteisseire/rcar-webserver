b984bd75f0a386f72313f1049eea5013e72de04c
jean-noel.teisseire <jean-noel.teisseire@smixin.com>
2020-02-17 16:58:37 +0100

    Remove console logs

--- app.js
+++ app.js
@@ -17,49 +17,41 @@ app.get("/", function(req, res) {
 });
 
 app.post("/upPress", function(req, res) {
-  console.log("upPress");
   up.writeSync(1);
   res.end();
 });
 
 app.post("/upRelease", function(req, res) {
-  console.log("upRelease");
   up.writeSync(0);
   res.end();
 });
 
 app.post("/downPress", function(req, res) {
-  console.log("downPress");
   down.writeSync(1);
   res.end();
 });
 
 app.post("/downRelease", function(req, res) {
-  console.log("downRelease");
   down.writeSync(0);
   res.end();
 });
 
 app.post("/leftPress", function(req, res) {
-  console.log("leftPress");
   left.writeSync(1);
   res.end();
 });
 
 app.post("/leftRelease", function(req, res) {
-  console.log("leftRelease");
   left.writeSync(0);
   res.end();
 });
 
 app.post("/rightPress", function(req, res) {
-  console.log("rightPress");
   right.writeSync(1);
   res.end();
 });
 
 app.post("/rightRelease", function(req, res) {
-  console.log("rightRelease");
   right.writeSync(0);
   res.end();
 });
